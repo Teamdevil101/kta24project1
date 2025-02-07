39e59122c5b9b437f724b09d29f7081bb0f6b1eb
Teamdevil101 <rivoaidla101@gmail.com>
2025-02-07 11:35:44 +0200

    Update script.js with useful stuff

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('I had to merge this branch with main because feature1 was created earlier.');
+console.log(foo);
+var foo = 'Foo'
+
+function Foobar(input)
+{
+    return foo + input
+}
+
+let bar = Foobar("bar");
+console.log(bar);
+
+let fizzbuzz = function (amount)
+{
+    for (let i = 0; i < amount; i++) {
+        
+        let result = "";
+        if(i % 3 == 0)
+            result += "Fizz";
+        if(i % 5 == 0)
+            result += "Buzz";
+        
+        if(i == 0 || result == "")
+            result = i;
+        
+        console.log(result);
+    }
+
+    return "Done!";
+}
+
+let person = {
+    name: 'Alex',
+    age: 9,
+    occupation: 'Domestic Terrorist',
+    die: () => {
+        return "dead";
+    }
+}
+
+console.log(fizzbuzz(24));
+console.log(person);
